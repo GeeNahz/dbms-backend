65bdc29031809fbc49d022cfcc528e2b1416cefc
Godswill Nasara <godswillnlm@gmail.com>
2023-09-11 01:05:08 +0100

    Update: include dbms_app to installed apps

--- dbms_backend/settings/config.py
+++ dbms_backend/settings/config.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 INSTALLED_APPS = [
     "rest_framework",
+    "dbms_app.apps.DbmsAppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
