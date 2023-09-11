a701496159749d96ee2bae967008c527522fa820
Godswill Nasara <godswillnlm@gmail.com>
2023-09-11 17:34:20 +0100

    Feature: create auth routes

--- dbms_app/urls.py
+++ dbms_app/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    RegisterView,
+    LoginAPIView,
+    LogoutAPIView,
+)
 
 app_name = "dbms_app"
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
