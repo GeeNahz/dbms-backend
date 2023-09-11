337cbe4e8fc679fcfec17de05525284b11d9f74e
Godswill Nasara <godswillnlm@gmail.com>
2023-09-11 01:19:21 +0100

    Update: write comments to indicate what TODO

--- dbms_app/models.py
+++ dbms_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO: Create Student model
+
+# TODO: Create Staff model
+
+# TODO: Create Course model
+
+# TODO: Create ResearchProject model
+
+# TODO: Create AdminResponsibility model
+
+# TODO: Create PerformanceEvaluation model
+
+# TODO: Create Department model
+
+# TODO: Create Faculty model
+
+# TODO: Create University model
