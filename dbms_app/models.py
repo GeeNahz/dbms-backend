8df4aee324895efbd4d4114b609fb83ed16a267a
Godswill Nasara <godswillnlm@gmail.com>
2023-09-11 17:33:02 +0100

    Feature: create an extended user model

--- dbms_app/models.py
+++ dbms_app/models.py
@@ -1,6 +1,31 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    """
+    Extended User model to create tokens
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    middle_name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=17, unique=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+    
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
 
 # TODO: Create Student model
 
